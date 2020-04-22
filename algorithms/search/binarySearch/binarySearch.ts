4430098ba92013f0f962f7a982d762970603a392
alexbrylov1994 <alexbrylov1994@hotmail.com>
2020-04-21 22:29:58 -0600

    implemented 2 version of binary search

--- algorithms/search/binarySearch/binarySearch.ts
+++ algorithms/search/binarySearch/binarySearch.ts
@@ -30,23 +30,21 @@ export function binarySearch(array: number[], key: number): number {
   return -1;
 }
 
-export function binarySearchRecursive(array: number[], key: number, end: number, start: number): number {
+export function binarySearchRecursive(array: number[], key: number, start: number, end: number): number {
+  if (start <= end) {
+    let middle: number = start + Math.floor((end - start) / 2);
 
-  // while (start <= end) {
-  //   // or Math.floor((end + start) / 2)
-  //   let middle: number = start + Math.floor((end - start) / 2);
-
-  //   // if middle element, return it
-  //   if (array[middle] === key) {
-  //     return middle;
-  //   }
-  //   // update pointer based on if we go left or right in array
-  //   if (array[middle] < key) {
-  //     start = middle + 1;
-  //   } else {
-  //     end = middle - 1;
-  //   }
-  // }
-
-  return -1;
+    if (array[middle] === key) {
+      return middle;
+    }
+    if (array[middle] < key) {
+      start = middle + 1;
+      return binarySearchRecursive(array, key, start, end);
+    } else {
+      end = middle - 1;
+      return binarySearchRecursive(array, key, start, end);
+    }
+  } else {
+    return -1;
+  }
 }
--- index.ts
+++ index.ts
@@ -1,13 +1,2 @@
 // a place for you to
 // use 'npm run start'
-
-import { binarySearch, binarySearchRecursive } from "./algorithms/search/binarySearch/binarySearch";
-
-console.log("hello world");
-
-console.log(binarySearch([] , 5));
-console.log(binarySearch([1], 1));
-console.log(binarySearch([1, 2], 1));
-console.log(binarySearch([1, 5, 10, 12], 5));
-console.log(binarySearch([1, 5, 10, 12, 14, 17, 22, 100],17));
-console.log(binarySearch([1, 5, 10, 12, 14, 15, 17, 22, 100],17));
