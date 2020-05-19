0d5a8e6f1d473dd80d9bf874a3de18cbe14dd5e4
alexbrylov1994 <alexbrylov1994@hotmail.com>
2020-05-18 21:08:28 -0600

    insertion sort done

---
+++ algorithms/sort/insertionSort/insertionSort.ts
@@ -0,0 +1,18 @@
+export function insertionSort(array: number[]): number[] {
+    for(let i=0; i < array.length; i++) {
+        for(let cur=i; cur >= 0; cur--) {
+            if (array[cur] < array[cur-1]) {
+                swap(array, cur-1, cur);
+            }
+        }
+    }
+    return array;
+}
+
+const swap = (arr: number[], i1, i2): void => {
+    const first = arr[i2];    
+    const second = arr[i1];
+    
+    arr[i1]=first;
+    arr[i2]=second;
+}
