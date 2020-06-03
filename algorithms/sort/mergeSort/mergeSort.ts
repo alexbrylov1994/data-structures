f49a8666c9378e15d5f17d6318f56459a5a97ec4
alexbrylov1994 <alexbrylov1994@hotmail.com>
2020-06-02 21:15:44 -0600

    finished mergeSort

--- algorithms/sort/mergeSort/mergeSort.ts
+++ algorithms/sort/mergeSort/mergeSort.ts
@@ -1,17 +1,40 @@
 export function mergeSort(array: number[]): number[] {
     // base case 
-    if (array.length <= 1) 
+    if (array.length <= 1)
         return array;
 
-    const mid = Math.floor((array.length)/2);
+    const mid = Math.floor(array.length / 2);
     const left = array.slice(0, mid);
     const right = array.slice(mid, array.length);
-
-
-    return merge(left,right);
+    
+    return merge(mergeSort(left), mergeSort(right));
 }
 
 function merge(left: number[], right: number[]): number[] {
+    const array = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
 
-    return [];
+    while(leftIndex < left.length || rightIndex < right.length) {
+        let leftElement = left[leftIndex];
+        let rightElement = right[rightIndex];
+
+        if (!leftElement) {
+            array.push(rightElement);
+            rightIndex++;
+        } else if (!rightElement) {
+            array.push(leftElement);
+            leftIndex++;
+        } else if (leftElement < rightElement) {
+            array.push(leftElement);
+            leftIndex++;
+        } else {
+            array.push(rightElement);
+            rightIndex++;
+        }
+
+    }
+    console.log(left,right)
+    console.log('result', array);
+    return array;
 }
