c9808c57ba0636256152a3f89323309f2f849273
alexbrylov1994 <alexbrylov1994@hotmail.com>
2020-11-28 00:38:52 -0700

    simpler solution

--- common-problemms/countUniqueValues.ts
+++ common-problemms/countUniqueValues.ts
@@ -1,20 +1,25 @@
 const countUniqueValue = (array: number[]): number => {
-    let start = 0;
-    let next = start + 1;
+    let current = 0;
+    let next = current + 1;
 
     // if empty -> 0
     if (!array.length) {
         return 0;
     }
 
+    let count = 1; // since we must have at least 1 unique
+    // and in case we have 1 element logic won't work
+    // and when we have current at the end of an array, next is undefined and it's a unique value
+    // when 2+ elements
     while (next < array.length) {
-        if (array[start] !== array[next]) {
-            start++;
-            array[start] = array[next]
+        if (array[current] !== array[next]) {
+            count++;
+            current = next;
+            next++;
         } else {
-            next++
+            next++;
         }
     }
 
-    return start + 1;
+    return count;
 }
