7aa7c07d3a664ced2eb05418a75555a82e68ebf6
alexbrylov1994 <alexbrylov1994@hotmail.com>
2020-11-29 21:56:25 -0700

    max sub array

---
+++ common-problemms/maxSubarraySum.ts
@@ -0,0 +1,21 @@
+const maxSubarraySum = (array: number[], num: number) => {
+    let maxSum = 0;
+    let tempSum = 0;
+
+    if (array.length < num)
+        return null;
+
+    for (let i = 0; i < num; i++) {
+        maxSum += array[i];
+    }
+
+    tempSum = maxSum;
+
+    for (let i = num; i < array.length; i++) {
+        tempSum = tempSum - array[i - num] + array[i];
+        maxSum = Math.max(maxSum, tempSum);
+    }
+
+    return maxSum;
+}
+
