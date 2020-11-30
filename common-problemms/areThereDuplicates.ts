b209a0242124cf7a75baff06ed8aaaff6a238f50
alexbrylov1994 <alexbrylov1994@hotmail.com>
2020-11-29 22:23:07 -0700

    check if there are any duplicates among the arguments

---
+++ common-problemms/areThereDuplicates.ts
@@ -0,0 +1,14 @@
+const areThereDuplicates = (...args: string[] | number[]): boolean => {
+
+    let hash = {};
+
+    for (let element of args) {
+        if (hash[element] || hash[element] === 0) {
+            return true
+        } else {
+            hash[element] = 1;
+        }
+    }
+
+    return false;
+}
