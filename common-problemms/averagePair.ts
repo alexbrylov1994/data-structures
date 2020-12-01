0bf14ad009241def98b71e745cf74351194e7eb8
alexbrylov1994 <alexbrylov1994@hotmail.com>
2020-11-30 22:09:45 -0700

    determine if there is a pair of values where their average equals to target average

---
+++ common-problemms/averagePair.ts
@@ -0,0 +1,30 @@
+const averagePair = (arr: [], target: number): boolean => {
+    // if no array -> return false 
+
+    // have pointers at the start and end
+    // add the numbers
+    // if number bigger than target, move end pointer left
+    // if number is smaller than target, start pointer right
+
+
+    if (arr.length < 2) {
+        return false;
+    }
+
+    let start = 0;
+    let end = arr.length - 1;
+
+    while (start < end) {
+        let average = (arr[start] + arr[end]) / 2;
+
+        if (average === target) {
+            return true;
+        } else if (average < target) {
+            start++
+        } else if (average > target) {
+            end--;
+        }
+    }
+
+    return false;
+}
