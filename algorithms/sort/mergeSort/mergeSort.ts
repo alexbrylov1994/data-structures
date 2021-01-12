598e034476e9f6dc09b6cc5f6cb4945ba136d982
alexbrylov1994 <alexbrylov1994@hotmail.com>
2021-01-12 00:24:11 -0700

    merge sort finally done

--- algorithms/sort/mergeSort/mergeSort.ts
+++ algorithms/sort/mergeSort/mergeSort.ts
@@ -1,41 +1,42 @@
 export function mergeSort(array: number[]): number[] {
-    // base case 
-    if (array.length <= 1)
+    if (array.length <= 1) {
         return array;
+    }
 
     const mid = Math.floor(array.length / 2);
     const left = array.slice(0, mid);
     const right = array.slice(mid, array.length);
-    console.log('before merge:', left,right);
+
     return merge(mergeSort(left), mergeSort(right));
 }
 
 const merge = (left: number[], right: number[]): number[] => {
-    const array = [];
+    let array = [];
     let leftIndex = 0;
     let rightIndex = 0;
 
-    while(leftIndex < left.length && rightIndex < right.length) {
+    while (leftIndex < left.length && rightIndex < right.length) {
         let leftElement = left[leftIndex];
         let rightElement = right[rightIndex];
-        console.log( 'leftIndex', leftIndex);
-        console.log( 'rightIndex', rightIndex);
-        if (!leftElement) {
-            array.push(rightElement);
-            rightIndex++;
-        } else if (!rightElement) {
-            array.push(leftElement);
-            leftIndex++;
-        } else if (leftElement < rightElement) {
+
+        if (leftElement < rightElement) {
             array.push(leftElement);
             leftIndex++;
         } else {
             array.push(rightElement);
             rightIndex++;
         }
-
     }
-    console.log('merging',left,right)
-    console.log('result', array);
+
+    while (leftIndex < left.length) {
+        array.push(left[leftIndex]);
+        leftIndex++;
+    }
+
+    while (rightIndex < right.length) {
+        array.push(right[rightIndex]);
+        rightIndex++;
+    }
+
     return array;
 }
