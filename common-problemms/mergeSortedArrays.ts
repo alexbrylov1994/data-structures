a5cbf1186ab2f39055da894189e388d7713d1f31
alexbrylov1994 <alexbrylov1994@hotmail.com>
2021-02-24 22:44:43 -0700

    added merge for sorted arrays

---
+++ common-problemms/mergeSortedArrays.ts
@@ -0,0 +1,31 @@
+const merge = (left: number[], right: number[]): number[] => {
+    let array = [];
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while (leftIndex < left.length && rightIndex < right.length) {
+        let leftElement = left[leftIndex];
+        let rightElement = right[rightIndex];
+
+        if (leftElement < rightElement) {
+            array.push(leftElement);
+            leftIndex++;
+        } else {
+            array.push(rightElement);
+            rightIndex++;
+        }
+    }
+
+    while (leftIndex < left.length) {
+        array.push(left[leftIndex]);
+        leftIndex++;
+    }
+
+    while (rightIndex < right.length) {
+        array.push(right[rightIndex]);
+        rightIndex++;
+    }
+
+    return array;
+}
+
