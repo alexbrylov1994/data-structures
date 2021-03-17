445f99880e703b07c7ee2e0ae2a68fdd9ad432d6
alexbrylov1994 <alexbrylov1994@hotmail.com>
2021-03-16 23:56:40 -0600

    added prepend for doubly linked list

--- data-structures/doublyLinkedList/doublyLinkedList.ts
+++ data-structures/doublyLinkedList/doublyLinkedList.ts
@@ -23,6 +23,61 @@ export default class DoublyLinkedList {
     }
 
     prepend(value) {
+        const newNode = new DoublyLinkedListNode(value);
 
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+            return;
+        }
+        this.head.previous = newNode;
+        newNode.next = this.head;
+        this.head = newNode;
+    }
+
+    deleteTail() {
+        if (!this.tail) {
+            // No tail to delete.
+            return null;
+        }
+        const deletedTail = this.tail;
+
+        if (this.head === this.tail) {
+            // There is only one node in linked list.
+            this.head = null;
+            this.tail = null;
+            return deletedTail;
+        }
+        // If there are many nodes in linked list...
+        this.tail = this.tail.previous;
+        this.tail.next = null;
+
+        return deletedTail;
+    }
+
+    deleteHead() {
+        if (!this.head) {
+            return null;
+        }
+
+        const deletedHead = this.head;
+
+        if (this.head.next) {
+            this.head = this.head.next;
+            this.head.previous = null;
+        } else {
+            this.head = null;
+            this.tail = null;
+        }
+
+        return deletedHead;
     }
 }
+
+const list = new DoublyLinkedList();
+list.append(1);
+list.append(2);
+list.append(3);
+
+list.prepend(0);
+console.log(list);
