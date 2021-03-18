e6fe77c295619fa22a91a4aa62673bcb05078a81
alexbrylov1994 <alexbrylov1994@hotmail.com>
2021-03-17 21:41:06 -0600

    finished doubly linked list

--- data-structures/doublyLinkedList/doublyLinkedList.ts
+++ data-structures/doublyLinkedList/doublyLinkedList.ts
@@ -72,12 +72,150 @@ export default class DoublyLinkedList {
 
         return deletedHead;
     }
+
+    find(value) {
+        if (!this.head) {
+            return null
+        }
+
+        let currentNode = this.head;
+
+        while (currentNode) {
+            if (currentNode.value === value) {
+                return currentNode;
+            } else {
+                currentNode = currentNode.next;
+            }
+        }
+
+        return null;
+    }
+
+    insert(index, value) {
+        let currentNode = this.head;
+        let i = 0;
+
+        if (!this.head) {
+            return null;
+        }
+
+        if (index === 0) {
+            const newNode = new DoublyLinkedListNode(value);
+
+            newNode.next = this.head;
+            this.head.previous = newNode;
+
+            this.head = newNode;
+            return;
+        }
+
+        while (i < index && currentNode) {
+            i++;
+            currentNode = currentNode.next;
+        }
+
+        if (i < index) {
+            return null;
+        } else {
+            const newNode = new DoublyLinkedListNode(value);
+            const prevNode = currentNode.previous;
+            prevNode.next = newNode;
+            newNode.previous = prevNode;
+
+            newNode.next = currentNode
+            currentNode.previous = newNode;
+
+        }
+
+    }
+
+    update(index, value) {
+        let currentNode = this.head;
+        let i = index;
+
+        while (i < index && currentNode) {
+            i++;
+            currentNode = currentNode.next;
+        }
+
+        if (i < index) {
+            return null;
+        } else {
+            currentNode.value = value;
+        }
+    }
+
+    reverse() {
+        let currNode = this.head;
+        let prevNode = null;
+        let nextNode = null;
+
+        while (currNode) {
+            // Store next node.
+            nextNode = currNode.next;
+            prevNode = currNode.previous;
+
+            // Change next node of the current node so it would link to previous node.
+            currNode.next = prevNode;
+            currNode.previous = nextNode;
+
+            // Move prevNode and currNode nodes one step forward.
+            prevNode = currNode;
+            currNode = nextNode;
+        }
+
+        // Reset head and tail.
+        this.tail = this.head;
+        this.head = prevNode;
+    }
+
+    delete(value) {
+        if (!this.head) {
+            return null;
+        }
+
+        let deletedNode = null;
+        let currentNode = this.head;
+
+        while (currentNode) {
+            if (currentNode.value === value) {
+                deletedNode = currentNode;
+
+                if (deletedNode === this.head) {
+                    // If HEAD is going to be deleted...
+
+                    // Set head to second node, which will become new head.
+                    this.head = deletedNode.next;
+
+                    // Set new head's previous to null.
+                    if (this.head) {
+                        this.head.previous = null;
+                    }
+
+                    // If all the nodes in list has same value that is passed as argument
+                    // then all nodes will get deleted, therefore tail needs to be updated.
+                    if (deletedNode === this.tail) {
+                        this.tail = null;
+                    }
+                } else if (deletedNode === this.tail) {
+                    // If TAIL is going to be deleted...
+
+                    // Set tail to second last node, which will become new tail.
+                    this.tail = deletedNode.previous;
+                    this.tail.next = null;
+                } else {
+                    // If MIDDLE node is going to be deleted...
+                    const previousNode = deletedNode.previous;
+                    const nextNode = deletedNode.next;
+
+                    previousNode.next = nextNode;
+                    nextNode.previous = previousNode;
+                }
+            }
+
+            currentNode = currentNode.next;
+        }
+
+        return deletedNode;
+    }
 }
-
-const list = new DoublyLinkedList();
-list.append(1);
-list.append(2);
-list.append(3);
-
-list.prepend(0);
-console.log(list);
