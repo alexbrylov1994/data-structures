0abfd571d014904de5c86488a124658e23f3e404
alexbrylov1994 <alexbrylov1994@hotmail.com>
2021-04-02 14:26:27 -0600

    BFS added

---
+++ algorithms/Traversal/breadthFirstSearch.ts
@@ -0,0 +1,45 @@
+import BinarySearchTree from '../../data-structures/binarySearchTree/binarySearchTree';
+import BinaryTreeNode from '../../data-structures/binarySearchTree/binarySearchTreeNode';
+
+class Queue {
+    queue: BinaryTreeNode[];
+    constructor() {
+        this.queue = [];
+    }
+
+    enqueue(value): void {
+        this.queue.push(value);
+    }
+
+    dequeue(): BinaryTreeNode {
+        return this.queue.shift();
+    }
+
+    length(): number {
+        return this.queue.length;
+    }
+}
+
+
+export default function breadthFirstSearch(rootNode: BinaryTreeNode): number[] {
+
+    const nodeQueue = new Queue();
+    const visited = [];
+
+    nodeQueue.enqueue(rootNode);
+    console.log('hello');
+    while (nodeQueue.length()) {
+        console.log('nim:', nodeQueue.length());
+        const currentNode = nodeQueue.dequeue();
+        visited.push(currentNode.value);
+
+        if (currentNode.left) {
+            nodeQueue.enqueue(currentNode.left);
+        }
+        if (currentNode.right) {
+            nodeQueue.enqueue(currentNode.right);
+        }
+    }
+
+    return visited;
+}
