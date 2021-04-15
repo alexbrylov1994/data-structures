cbda1c502ac4d5a980a4a9c29ba0d21ffc145e1c
alexbrylov1994 <alexbrylov1994@hotmail.com>
2021-04-15 16:36:51 -0600

    fixed

--- algorithms/recursion/fibonachi.ts
+++ algorithms/recursion/fibonachi.ts
@@ -1,7 +1,12 @@
 const fib = (pos: number): number => {
-    if (pos < 2) {
+    if (pos <= 0) {
+        return 0;
+    }
+    if (pos === 1) {
         return 1;
     }
 
     return fib(pos - 1) + fib(pos - 2);
 }
+
+console.log(fib(3));
