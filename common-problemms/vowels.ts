4d03e0b75025220fa67d13f8bdf11cd15a6c75f8
alexbrylov1994 <alexbrylov1994@hotmail.com>
2021-04-26 11:30:02 -0600

    added problem

---
+++ common-problemms/vowels.ts
@@ -0,0 +1,13 @@
+const vowels = (str: string) => {
+    let count = 0;
+
+    const checker = ['a', 'e', 'i', 'o', 'u'];
+
+    for (let char of str.toLowerCase()) {
+        if (checker.includes(char)) {
+            count++;
+        }
+    }
+
+    return count;
+}
