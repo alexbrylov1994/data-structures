8bca61fa7bd59b0d7ab6391275c5fc22f40e6f31
alexbrylov1994 <alexbrylov1994@hotmail.com>
2021-05-25 22:06:07 -0600

    added a problem

---
+++ leetcode/minRemoveToMakeValid.ts
@@ -0,0 +1,53 @@
+// 1249. Minimum Remove to Make Valid Parentheses
+
+// Given a string s of '(' , ')' and lowercase English characters. 
+
+// Your task is to remove the minimum number of parentheses ( '(' or ')', in any positions ) so that the resulting parentheses string is valid and return any valid string.
+
+// Formally, a parentheses string is valid if and only if:
+
+// It is the empty string, contains only lowercase characters, or
+// It can be written as AB (A concatenated with B), where A and B are valid strings, or
+// It can be written as (A), where A is a valid string.
+
+
+// Example 1:
+
+// Input: s = "lee(t(c)o)de)"
+// Output: "lee(t(c)o)de"
+// Explanation: "lee(t(co)de)" , "lee(t(c)ode)" would also be accepted.
+// Example 2:
+
+// Input: s = "a)b(c)d"
+// Output: "ab(c)d"
+// Example 3:
+
+// Input: s = "))(("
+// Output: ""
+// Explanation: An empty string is also valid.
+// Example 4:
+
+// Input: s = "(a(b(c)d)"
+// Output: "a(b(c)d)"
+
+function minRemoveToMakeValid(s: string): string {
+    let res = s.split('');
+    let stack = [];
+
+    for (let i = 0; i < res.length; i++) {
+        if (res[i] === '(') {
+            stack.push(i);
+        } else if (res[i] === ')' && stack.length) {
+            stack.pop();
+        } else if (res[i] === ')') {
+            res[i] = '';
+        }
+    }
+
+    while (stack.length) {
+        let index = stack.pop();
+        res[index] = '';
+    }
+
+    return res.join('');
+};
