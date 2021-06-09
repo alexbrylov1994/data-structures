f55b69e907031cbe727592f709617081980d9273
alexbrylov1994 <alexbrylov1994@hotmail.com>
2021-06-08 23:18:47 -0600

    added a qyestion

---
+++ leetcode/levelOrder.ts
@@ -0,0 +1,99 @@
+// 102. Binary Tree Level Order Traversal
+
+// Given the root of a binary tree, return the level order traversal of its nodes' values. (i.e., from left to right, level by level).
+
+// Example 1:
+
+// Input: root = [3,9,20,null,null,15,7]
+// Output: [[3],[9,20],[15,7]]
+
+// Example 2:
+
+// Input: root = [1]
+// Output: [[1]]
+
+// Example 3:
+
+// Input: root = []
+// Output: []
+
+// Definition for a binary tree node.
+class TreeNode {
+    val: number
+    left: TreeNode | null
+    right: TreeNode | null
+    constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+        this.val = (val === undefined ? 0 : val)
+        this.left = (left === undefined ? null : left)
+        this.right = (right === undefined ? null : right)
+    }
+}
+
+
+function levelOrder(root): number[][] {
+
+    if (!root) {
+        return [];
+    }
+
+    let result = [[]];
+    let index = 0;
+    let queue = [root];
+    queue.push('stop');
+
+    while (queue.length) {
+        let element = queue.shift();
+
+        if (element === 'stop') {
+            if (!queue.length) {
+                break;
+            }
+            queue.push(element);
+            result.push([]);
+            index++;
+        } else {
+            result[index].push(element.val);
+            if (element.left) {
+                queue.push(element.left)
+            }
+
+            if (element.right) {
+                queue.push(element.right)
+            }
+        }
+    }
+
+    return result;
+};
+
+// ------- 2nd Solution -------
+const levelOrder2 = function (root) {
+    if (!root) return [];
+    const result = [];
+    const queue = [root];
+
+    while (queue.length) {
+        const currentLevelValues = [];
+        let length = queue.length, count = 0;
+
+        while (count < length) {
+            const currentNode = queue.shift();
+
+            currentLevelValues.push(currentNode.value);
+
+            if (currentNode.left) {
+                queue.push(currentNode.left)
+            }
+
+            if (currentNode.right) {
+                queue.push(currentNode.right)
+            }
+
+            count++;
+        }
+
+        result.push(currentLevelValues);
+    }
+
+    return result;
+};
