ad4c121428e5ca2ed48a5126e3bb9c14bea26606
alexbrylov1994 <alexbrylov1994@hotmail.com>
2021-07-14 00:44:37 -0600

    added one more

---
+++ leetcode/Amazon/canAttendMeetings.ts
@@ -0,0 +1,32 @@
+// 252. Meeting Rooms
+
+// Given an array of meeting time intervals where intervals[i] = [starti, endi], determine if a person could attend all meetings.
+
+// Example 1:
+
+// Input: intervals = [[0,30],[5,10],[15,20]]
+// Output: false
+// Example 2:
+
+// Input: intervals = [[7,10],[2,4]]
+// Output: true
+
+function canAttendMeetings(intervals: number[][]): boolean {
+    if (intervals.length <= 1) {
+        return true;
+    }
+
+    intervals = intervals.sort((a, b) => a[0] - b[0]);
+
+    let start = intervals[0];
+
+    for (let i = 1; i < intervals.length; i++) {
+        if (start[1] > intervals[i][0]) {
+            return false;
+        } else {
+            start = intervals[i];
+        }
+    }
+
+    return true;
+};
