53c441a2c1c77f9322c7c095388182bdea0b1b04
alexbrylov1994 <alexbrylov1994@hotmail.com>
2021-07-26 01:45:36 -0600

    fixed max heap

--- data-structures/binaryHeap/maxHeap.ts
+++ data-structures/binaryHeap/maxHeap.ts
@@ -2,20 +2,20 @@ export default class MaxBinaryHeap {
     // 2n+1 left child
     // 2n+2 right child 
     // (n-1)/2 floor, parent
-    values: number[];
+    heap: number[];
     constructor() {
-        this.values = [];
+        this.heap = [];
         // this.values = [55, 39, 41, 18, 27, 12, 33];
     }
 
     insert(value) {
-        this.values.push(value);
+        this.heap.push(value);
 
-        if (this.values.length <= 1) {
+        if (this.heap.length <= 1) {
             return;
         }
 
-        this.bubbleUp(value, (this.values.length - 1))
+        this.bubbleUp(value, (this.heap.length - 1))
     }
 
     private bubbleUp(value, index) {
@@ -23,62 +23,73 @@ export default class MaxBinaryHeap {
             return;
         }
 
-        let parentIndex = this.getParentIndex(index);
-        if (value > this.values[parentIndex]) {
-            [this.values[index], this.values[parentIndex]] = [this.values[parentIndex], this.values[index]]
+        let parentIndex = this.getParent(index);
+        if (value > this.heap[parentIndex]) {
+            [this.heap[index], this.heap[parentIndex]] = [this.heap[parentIndex], this.heap[index]]
             this.bubbleUp(value, parentIndex);
         }
 
     }
 
-    getParentIndex(index): number {
-        return Math.floor((index - 1) / 2);
+    getLeftChild(index: number): number {
+        let child = (index * 2) + 1;
+        return child < this.heap.length ? child : null;
     }
 
-    getLeftChild(index): number {
-        return (2 * index + 1);
+    getRightChild(index: number): number {
+        let child = (index * 2) + 2;
+        return child < this.heap.length ? child : null;
     }
 
-    getRightChild(index): number {
-        return (2 * index + 2);
+    getParent(index: number): number {
+        let parent = Math.floor((index - 1) / 2);
+        return parent >= 0 ? parent : null;
     }
 
-    extractMax() {
+    extractMax(): number {
 
-        if (!this.values.length) {
-            return;
-        } else if (this.values.length <= 2) {
-            // if we have 1 element, becomes empty
-            // if we have 2 elements, 2nd element becomes root
-            this.values.shift();
-            return;
-        }
-
-        [this.values[0], this.values[this.values.length - 1]] = [this.values[this.values.length - 1], this.values[0]]
-        this.values.pop();
-
+        [this.heap[0], this.heap[this.heap.length - 1]] = [this.heap[this.heap.length - 1], this.heap[0]];
+        let max = this.heap.pop();
         this.bubleDown(0);
+        return max;
     }
 
     bubleDown(index) {
-        if (this.getLeftChild(index) > (this.values.length - 1) && this.getRightChild(index) > (this.values.length - 1)) {
+        if (!this.getRightChild(index) && !this.getLeftChild(index)) {
             return;
         }
 
-        let greaterChild;
-        if (this.getLeftChild(index) < (this.values.length - 1) && this.getRightChild(index) < (this.values.length - 1)) {
-            if (this.values[this.getLeftChild(index)] > this.values[this.getRightChild(index)]) {
-                greaterChild = this.getLeftChild(index);
-            } else {
-                greaterChild = this.getRightChild(index);
-            }
-        } else if (this.getLeftChild(index) < (this.values.length - 1)) {
-            greaterChild = this.getLeftChild(index);
+        let child;
+        if (this.heap[this.getLeftChild(index)] > this.heap[this.getRightChild(index)] || !this.getRightChild(index)) {
+            child = this.getLeftChild(index);
+        } else {
+            child = this.getRightChild(index);
         }
 
-        if (this.values[index] < this.values[greaterChild]) {
-            [this.values[index], this.values[greaterChild]] = [this.values[greaterChild], this.values[index]];
-            this.bubleDown(greaterChild);
+        if (this.heap[child] > this.heap[index]) {
+            [this.heap[index], this.heap[child]] = [this.heap[child], this.heap[index]];
+            this.bubleDown(child);
         }
     }
 }
+
+let h = new MaxBinaryHeap();
+
+h.insert(1);
+h.insert(2);
+h.insert(3);
+h.insert(4);
+h.insert(5);
+h.insert(6);
+h.insert(7);
+h.insert(8);
+h.insert(9);
+console.log(h.extractMax());
+console.log(h.extractMax());
+console.log(h.extractMax());
+console.log(h.extractMax());
+console.log(h.extractMax());
+console.log(h.extractMax());
+console.log(h.extractMax());
+console.log(h.extractMax());
+console.log(h.extractMax());
