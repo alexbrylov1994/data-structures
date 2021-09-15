9a9915c5a0c43ce4cb2dcf4a0dbcac3a02a28ffc
alexbrylov1994 <alexbrylov1994@hotmail.com>
2021-09-15 14:48:36 -0600

    1

--- leetcode/Amazon/distanceK.ts
+++ leetcode/Amazon/distanceK.ts
@@ -33,31 +33,31 @@ class TreeNode {
 //    7  4
 // if tartget is 5 and we look for 2 away, we will get 7,4 and 1 (5-3-1)
 function distanceK(root: TreeNode | null, target: TreeNode | null, k: number): number[] {
-    if (!root) return []
-    const node = findTarget(root, null, target)
-    const res = []
-    findAllKApart(node, k, res)
-    return res
+    if (!root) return [];
+    const node = findTarget(root, null, target);
+    const res = [];
+    findAllKApart(node, k, res);
+    return res;
 };
 
 function findTarget(root, parent, target) {
-    if (!root) return null
-    root.parent = parent
+    if (!root) return null;
+    root.parent = parent;
     if (root === target) {
-        return root
+        return root;
     }
-    return findTarget(root.left, root, target) || findTarget(root.right, root, target)
+    return findTarget(root.left, root, target) || findTarget(root.right, root, target);
 }
 
 function findAllKApart(root, k, res) {
-    if (!root || root.visited) return res
+    if (!root || root.visited) return res;
     if (k == 0) {
-        res.push(root.val)
-        return res
+        res.push(root.val);
+        return res;
     }
     root.visited = true
-    findAllKApart(root.left, k - 1, res)
-    findAllKApart(root.right, k - 1, res)
-    findAllKApart(root.parent, k - 1, res)
-    return res
+    findAllKApart(root.left, k - 1, res);
+    findAllKApart(root.right, k - 1, res);
+    findAllKApart(root.parent, k - 1, res);
+    return res;
 }
