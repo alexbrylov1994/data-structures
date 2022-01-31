3afe3d61311990542a8f130219022c21dfa2c0de
alexbrylov1994 <alexbrylov1994@hotmail.com>
2022-01-31 01:18:36 -0700

    1

---
+++ leetcode/genericQuestions/exist.ts
@@ -0,0 +1,91 @@
+// 79. Word Search
+
+// Given an m x n grid of characters board and a string word, return true if word exists in the grid.
+
+// The word can be constructed from letters of sequentially adjacent cells, where adjacent cells are horizontally or vertically neighboring. The same letter cell may not be used more than once.
+
+// Example 1:
+
+// Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+// Output: true
+// Example 2:
+
+
+// Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "SEE"
+// Output: true
+// Example 3:
+
+
+// Input: board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCB"
+// Output: false
+
+function exist(board: string[][], word: string): boolean {
+    let seen = new Set();
+    var dfs = function (row, col, i) {
+        if (i === word.length - 1) {
+            return true;
+        }
+
+        if (row < 0 || row >= board.length || col < 0 || col >= board[0].length || seen.has([row, col]) || word[i] !== board[row][col]) {
+            return false;
+        }
+
+        // fix this, need to mark board and reset?
+        seen.add([row, col]);
+
+        // This can be done with strings:
+
+        // let s=new Set();
+        // s.add("1,1");
+        // s.add("2,2");
+        console.log('Add', seen);
+
+        return dfs(row + 1, col, i + 1) ||  dfs(row - 1, col, i + 1)  || dfs(row, col + 1, i + 1) || dfs(row, col -1, i + 1);
+    }
+
+    for (let row = 0; row < board.length; row++) {
+        for (let column = 0; column < board[0].length; column++) {
+            if (board[row][column] === word[0]) {
+                seen = new Set();
+                if (dfs(row, column, 0)) {
+                    return true;
+                }
+            }
+        }
+    }
+
+    return false;
+};
+
+// WORk!
+// var exist = function(board, word) {
+//     let result = false;
+//     var check = function(r, c, i) {
+//         if (!result) {
+//             if (r < 0 || c < 0 || r >= board.length || c >= board[0].length) return; // out of boundary
+//             if (board[r][c] != word[i]) return; // wrong character
+//             if (i == word.length - 1) { // got to the end means we found a correct path
+//                 result = true;
+//                 return;
+//             }
+//             board[r][c] = null; // mark our path so we dont go back and forth
+// 			// try all directions
+//             check(r+1,c,i+1)
+//             check(r-1,c,i+1)
+//             check(r,c+1,i+1)
+//             check(r,c-1,i+1)
+//             board[r][c] = word[i] // reset our board , very important
+//         }
+//     }
+
+//     for (let i=0;i<board.length;i++) {
+//         for (let j=0;j<board[0].length;j++) {
+//             if (board[i][j] == word[0]) {
+//                 check(i, j, 0)
+//                 if (result) return result;
+//             }
+//         }
+//     }
+    
+//     return result;
+// };
