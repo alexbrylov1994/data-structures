d2cb6e58960420f09ea69820e1b2eb03100c4c94
alexbrylov1994 <alexbrylov1994@hotmail.com>
2022-01-31 09:37:07 -0700

    1

--- leetcode/Microsoft/exist.ts
+++ leetcode/Microsoft/exist.ts
@@ -25,7 +25,7 @@ function exist(board: string[][], word: string): boolean {
             return false;
         }
 
-        if (i === word.length-1) {
+        if (i === word.length - 1) {
             return true;
         }
 
@@ -36,9 +36,9 @@ function exist(board: string[][], word: string): boolean {
         // let s=new Set();
         // s.add("1,1");
         // s.add("2,2");
-        board[row][col] = null 
-        console.log('X: ', row,' ', col);
-        if (dfs(row + 1, col, i + 1) ||  dfs(row - 1, col, i + 1)  || dfs(row, col + 1, i + 1) || dfs(row, col -1, i + 1)) {
+        board[row][col] = null
+        console.log('X: ', row, ' ', col);
+        if (dfs(row + 1, col, i + 1) || dfs(row - 1, col, i + 1) || dfs(row, col + 1, i + 1) || dfs(row, col - 1, i + 1)) {
             return true;
         }
 
@@ -61,34 +61,34 @@ function exist(board: string[][], word: string): boolean {
 };
 
 // WORk!
-// var exist = function(board, word) {
-//     let result = false;
-//     var check = function(r, c, i) {
-//         if (!result) {
-//             if (r < 0 || c < 0 || r >= board.length || c >= board[0].length) return; // out of boundary
-//             if (board[r][c] != word[i]) return; // wrong character
-//             if (i == word.length - 1) { // got to the end means we found a correct path
-//                 result = true;
-//                 return;
-//             }
-//             board[r][c] = null; // mark our path so we dont go back and forth
-// 			// try all directions
-//             check(r+1,c,i+1)
-//             check(r-1,c,i+1)
-//             check(r,c+1,i+1)
-//             check(r,c-1,i+1)
-//             board[r][c] = word[i] // reset our board , very important
-//         }
-//     }
+function wordExist(board: string[][], word: string): boolean {
+    let result = false;
+    var check = function (r, c, i) {
+        if (!result) {
+            if (r < 0 || c < 0 || r >= board.length || c >= board[0].length) return; // out of boundary
+            if (board[r][c] != word[i]) return; // wrong character
+            if (i == word.length - 1) { // got to the end means we found a correct path
+                result = true;
+                return;
+            }
+            board[r][c] = null; // mark our path so we dont go back and forth
+            // try all directions
+            check(r + 1, c, i + 1)
+            check(r - 1, c, i + 1)
+            check(r, c + 1, i + 1)
+            check(r, c - 1, i + 1)
+            board[r][c] = word[i] // reset our board , very important
+        }
+    }
 
-//     for (let i=0;i<board.length;i++) {
-//         for (let j=0;j<board[0].length;j++) {
-//             if (board[i][j] == word[0]) {
-//                 check(i, j, 0)
-//                 if (result) return result;
-//             }
-//         }
-//     }
-    
-//     return result;
-// };
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[0].length; j++) {
+            if (board[i][j] == word[0]) {
+                check(i, j, 0)
+                if (result) return result;
+            }
+        }
+    }
+
+    return result;
+};
